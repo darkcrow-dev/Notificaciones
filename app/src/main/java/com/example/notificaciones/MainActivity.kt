77220f479ead6f472436cdd50cb68f85461a9844
Charly Lopez <shadowlink1993@hotmail.com>
2022-12-16 23:05:13 -0600

    Aplicación en Kotlin para el manejo de notificaciones

--- app/src/main/java/com/example/notificaciones/MainActivity.kt
+++ app/src/main/java/com/example/notificaciones/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Boton_Notificaciones = findViewById(R.id.button_notification)
+        Boton_Notificaciones = findViewById(R.id.button_notification)       //Boton para las notificaciones
 
         crearCanalDeNotificacion()
 
